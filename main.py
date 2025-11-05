62068198ea32eb696611ba26c98052daca312725
sooyee0121-cloud <sooyee0121@gmail.com>
2025-11-05 09:25:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ a=st.text_input('안녕하세요! 만나서 반가워요!')
 st.selectbox('좋아하는 음식을 선택하세요!',['오페라케이크','된장찌개','까르보나라'])
 if st.button('인사말 생성):
              st.write(a+'님 안녕하세요! 반갑습니다!')
+st.write(b+'를 좋아하시는군요! 저도 좋아해요!')
+            st.error('반가워요!!')
+             st.balloons()
